b445a7c4cd48e15d57f1f09ca75214c0c704c49e
David <davidlawton90@yahoo.com>
2025-12-18 01:30:47 +1100

    day 7 update

--- 2025/day_7/src/main.zig
+++ 2025/day_7/src/main.zig
@@ -13,18 +13,23 @@ const height = input.len / width;
 var memo: [height][width-1]?usize = std.mem.zeroes([height][width-1]?usize);
 
 fn calculate_timelines(y: usize, x: usize) usize {
-    if (y >= height - 1) return 1;
     if (memo[y][x]) |cached| return cached;
     
     const char = input[x+y*width];
     
     const result: usize = switch(char) {
-        '.', 'S' => calculate_timelines(y+1, x),
-        '^' => blk: {
+        '.', 'S' => blk: { 
             var total: usize = 0;
-            if (x > 0) total += calculate_timelines(y, x - 1);
-            if (x + 1 < width - 1) total += calculate_timelines(y, x + 1);
-            break :blk total;
+            var next_y = y;
+            while (next_y < height - 1): (next_y+=1) {
+                const next_char = input[x+next_y*width];
+                if (next_char == '^') {
+                    if (x > 0) total += calculate_timelines(next_y, x - 1);
+                    if (x + 1 < width - 1) total += calculate_timelines(next_y, x + 1);
+                    break :blk total;
+                }
+            }
+            break :blk 1;
         },
         else => 0,
     };
@@ -43,7 +48,7 @@ pub fn main() !void {
 
     const start = std.mem.indexOf(u8, input, "S").?;
 
-    var visited: [height][width-1]bool =std.mem.zeroes([height][width-1]bool); 
+    var visited: [height][width-1]bool = std.mem.zeroes([height][width-1]bool);
 
     //manage unvisited nodes with stack dfs
     var stack = try std.ArrayList(Position).initCapacity(allocator, 16);
@@ -52,18 +57,20 @@ pub fn main() !void {
     
     var part1: usize = 0;
     while (stack.pop()) |beam| {
-        var y = beam.y;
+        visited[beam.y][beam.x] = true;
+        var y = beam.y+1;
         while (y < height) : (y += 1) {
-            visited[y][beam.x] = true;
             if (input[beam.x+y*width] == '^') {
                 var split: bool = false;
                 //spawn two new beams on either side of beam splitter
                 if (beam.x > 0 and visited[y][beam.x-1] == false) {
                     try stack.append(allocator, .{.y=y,.x=beam.x-1});
+                    visited[y][beam.x-1] = true; 
                     split = true;
                 }
                 if (beam.x+1 < width-1 and visited[y][beam.x+1] == false) {
                     try stack.append(allocator, .{.y=y, .x=beam.x+1});
+                    visited[y][beam.x-1] = true; 
                     split = true;
                 }
                 if (split) part1 += 1;
@@ -72,6 +79,7 @@ pub fn main() !void {
         }
 
     }
+
     const part2 = calculate_timelines(0, start);
 
     try stdout.print("Part 1: {} - Part 2: {}\n", .{part1, part2});
